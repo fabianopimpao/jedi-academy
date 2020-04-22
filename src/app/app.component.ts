f3f20e717cc94c9b11667e70a9d98671823193c5
Fabiano Pimpão <fabiano.pimpao@gmail.com>
2020-04-22 07:38:24 -0300

    Using ngFor and ngIf directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 
+import { Student } from './student/student.model';
+
 @Component({
   selector: 'jad-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  luke = {name: 'Luke', isJedi: true, temple: 'Coruscant'};
-  han = {name: 'Han Solo', isJedi: false};
-  leia = {name: 'Leia', isJedi: false};
+  
+  students: Student [] = [
+    {name: 'Luke', isJedi: true, temple: 'Coruscant'},
+    {name: 'Han Solo', isJedi: false, temple: ''},
+    {name: 'Leia', isJedi: false, temple: ''}
+  ]
 }
