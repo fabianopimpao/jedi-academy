f2201672c69a9e5afad68abe318e1542bd00dd82
Fabiano Pimpão <fabiano.pimpao@gmail.com>
2020-04-22 07:09:12 -0300

    Using @Input Decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'jad works!';
+  luke = {name: 'Luke', isJedi: true, temple: 'Coruscant'};
+  han = {name: 'Han Solo', isJedi: false};
+  leia = {name: 'Leia', isJedi: false};
 }
